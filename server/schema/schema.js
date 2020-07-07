f8a139fbc49b7a92684057e0b40e81c9d1235921
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-07-07 18:21:52 -0300

    the resolve

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require('graphql');
-
+const _ = require('lodash');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+let books = [
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  {name: 'The final empire', genre: 'Fantasy', id: '2'},
+  {name: 'The Long Earth', genre: 'Sci-fi', id: '3'},
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -19,6 +25,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: {type: GraphQLString} },
       resolve(parent, args){
         //code to get data from db / other source
+        return _.find(books, { id: args.id })
       }
     }
   }
