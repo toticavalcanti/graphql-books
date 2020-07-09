607de28111d29f00c9a0f8b00bd6be62dc5a75d0
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-07-08 21:14:10 -0300

    list books and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -58,6 +58,7 @@ const AuthorType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+
     book: {
       type: BookType,
       args: { id: {type: GraphQLID} },
@@ -66,11 +67,26 @@ const RootQuery = new GraphQLObjectType({
         return _.find(books, { id: args.id })
       }
     },
+
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID }},
       resolve(parent, args){
         return _.find(authors, { id: args.id })
+      }
+    },
+
+    books: {
+      type:GraphQLList(BookType),
+      resolve(parent, args){
+        return books
+      }
+    },
+
+    authors: {
+      type: GraphQLList(AuthorType),
+      resolve(parent, args){
+        return authors
       }
     }
   }
