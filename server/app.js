c615c78c1c31bd1277b302b4759f717969d4ad85
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-07-08 23:22:59 -0300

    node_modules remove

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose'); 
+
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://toticavalcanti:mongodb0401@cluster0.smvbm.mongodb.net/gql-ninja?retryWrites=true&w=majority', 
+  { useNewUrlParser: true, useUnifiedTopology: true }
+  
+)
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
