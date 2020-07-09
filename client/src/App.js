b604751c42274df742340d44688474026c354fe6
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-07-09 11:08:19 -0300

    create BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+//components
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="main">
       <h1>Reading List</h1>
+      <BookList />
     </div>
   );
 }
