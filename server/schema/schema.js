6403001564ad2066d37e5239099e7c5937aaf687
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-07-09 09:05:39 -0300

    list all books and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -37,6 +37,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve (parent, args){
         //return _.find(authors, {id: parent.authorId});
+        return Author.findById(parent.authorId);
       }
     }
   })
@@ -52,6 +53,7 @@ const AuthorType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve(parent, args){
         //return _.filter(books, {authorId: parent.id})
+        return Book.find({ authorId: parent.id});
       }
     }
   })
@@ -67,6 +69,7 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args){
         //code to get data from db / other source
         //return _.find(books, { id: args.id })
+        return Book.findById(args.id)
       }
     },
 
@@ -74,7 +77,8 @@ const RootQuery = new GraphQLObjectType({
       type: AuthorType,
       args: { id: { type: GraphQLID }},
       resolve(parent, args){
-        return _.find(authors, { id: args.id })
+        //return _.find(authors, { id: args.id })
+        return Author.findById(args.id);
       }
     },
 
@@ -82,6 +86,7 @@ const RootQuery = new GraphQLObjectType({
       type:GraphQLList(BookType),
       resolve(parent, args){
         //return books
+        return Book.find({});
       }
     },
 
@@ -89,6 +94,7 @@ const RootQuery = new GraphQLObjectType({
       type: GraphQLList(AuthorType),
       resolve(parent, args){
         //return authors
+        return Author.find({});
       }
     }
   }
